f335d5d26ca7d9fd9334c279e2a8e30c1b0b4678
unknown <81367175+clapbin@users.noreply.github.com>
2021-05-10 03:23:30 +0900

    scratch.py is updated by student2!

--- scratch.py
+++ scratch.py
@@ -1,7 +1,12 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
+
+
+data=[]
 for i in range(342, 372):
     URL = "https://comic.naver.com/webtoon/detail.nhn?titleId=552960&no=" + str(i) + "&weekday=fri"
     html = urlopen(URL)
@@ -9,4 +14,22 @@ for i in range(342, 372):
 
     rawrating = str(soup.find_all('strong'))
     rating = re.search("(\d\.\d\d)", rawrating)
-    print(rating.group())
+    pp=rating.group()
+
+    print('Rating : ' ,pp)
+    data.append(pp)
+
+#print(data)
+
+x = [342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,358,359,360,361,362,363,364,365,366,367,368,369,370,371]
+y = data
+plt.figure(figsize=(30,5))
+plt.xlabel('webtoon episode')
+plt.ylabel('Rating')
+plt.title('Changes in ratings of webtoons')
+ax=plt.axes()
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+plt.plot(x,y,'g')
+plt.xlim(342,371)
+plt.ylim(4,10)
+plt.show()
